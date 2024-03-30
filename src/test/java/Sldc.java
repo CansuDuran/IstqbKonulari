3d95d9f26ce415e5b2bc97def757dbabe8f366bf
CansuDuran <cansu.durann10@gmail.com>
2024-03-30 16:05:44 +0300

    Initial commit

---
+++ src/test/java/Sldc.java
@@ -0,0 +1,28 @@
+public class Sldc {
+
+/*
+*  SDLC (Yazılım Geliştirme Yaşam Döngüsü Nedir) ?
+SDLC mümkün olan en kısa sürede, en yüksek kalitede ve en düşük maliyetle yazılım üreten bir süreçtir. SDLC, bir organizasyonun iyi test edilmiş ve üretimde kullanıma hazır yüksek kaliteli yazılımı hızla üretmesine yardımcı olan iyi yapılandırılmış bir aşama akışı sağlar.
+SDLC altı aşamadan oluşur:
+*
+*1.Aşama: Planlama
+“Ne istiyoruz?” SDLC’ nin bu aşamasında ekip, analiz edilen gereksinimlerin uygulanması için gereken maliyeti ve kaynakları belirler. Ayrıca ilgili riskleri detaylandırır ve bu riskleri azaltmak için alt planlar sunar.
+Başka bir deyişle, ekip projenin fizibilitesini ve projeyi en düşük riski göz önünde bulundurarak nasıl başarılı bir şekilde uygulayabileceklerini belirlemelidir
+2. Aşama: Tasarım
+Ekip, ürün için geniş bir dizi gereksinim ve hedef üzerinde anlaşmaya vardıktan sonra, bir sonraki adım bir tasarım spesifikasyonu oluşturmak olacaktır. Bu, “Bunu nasıl inşa edeceğiz?” Sorusuna cevap vermelidir.
+Bir ürün ekibi için bu aşama, önerilen çalışmanın öncelik sırasının belirlenmesini, ürünün yol haritasının oluşturulması aşamasıdır. Bu, hem geliştirme hem de ürün ekiplerindeki herkesin amaçlarının daha net bir resmini elde etmesine yardımcı olacaktır.
+3.Aşama : Uygulama veya Kodlama
+Bu, mühendislik ekibinin ürünü fiilen kodladığı aşamadır. Bu aşamada geliştirme ekibi, yol haritasında iletilen üst düzey genel bakışı bir dizi taktiksel görev, bitiş tarihi ve günlük çalışma programlarına dönüştürür.
+4. Aşama: Test
+Ekip, yazılımın bir sürümünü tamamladıktan sonra, onu bir test ortamına yayınlayacak. Burada, QA ekibi ve geliştiriciler, herhangi bir kusur, hata veya diğer sorunları tespit etmek için uygulamanın tüm alanlarını test edecek.
+5. Aşama: Dağıtım
+Bu aşamada ekip, tüm kusurları düzelttiğinden ve yazılımın üzerinde anlaşmaya varılan hedeflere ve özelliklere göre oluşturulduğundan emindir.
+Şimdi yazılımı üretim ortamına bırakma zamanı. Bu, ürünün genel olarak müşterilerin satın alması ve kullanması için hazır olacağı anlamına gelir.
+6. Aşama: Bakım
+Yazılımın artık canlı olması ve müşteriler tarafından kullanılmasıyla birlikte, geliştirme ekibinin odak noktası onu korumaya kayacaktır.
+Geliştiricilerin iyileştirmeler, hata düzeltmeleri ve yeni özellikler için istekleri karşılamaya hazır olmaları gerekecektir. Bu talepler birçok kaynaktan (satışlar, yöneticiler, müşteriler) gelecek, ancak ürün yönetimi ekibi, geliştiricilerin üzerinde çalışması için bu girişimlerden hangisinin ürün yol haritasına gireceğini belirleyecektir.
+*
+* */
+
+}
+
